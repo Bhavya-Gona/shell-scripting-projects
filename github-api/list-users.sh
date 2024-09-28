b8fac4598df758aa2f0b50e71c7d76d9a37b896b
Bhavya-Gona <gonabhavyasri@gmail.com>
2024-09-28 01:27:51 -0700

    list-usersv1.sh

--- github-api/list-users.sh
+++ github-api/list-users.sh
@@ -3,6 +3,14 @@
 # GitHub API URL
 API_URL="https://api.github.com"
 
+#############
+## About:
+## Input:
+## owner:
+############
+
+helper()
+
 # GitHub username and personal access token
 USERNAME=$username
 TOKEN=$token
@@ -36,6 +44,12 @@ function list_users_with_read_access {
     fi
 }
 
+function helper{
+ expected_cmd_args=2
+ if [ $# -ne $expected_cmd_args]; then
+  echo "please execute the script with required cmd args"
+  echo "asd"
+ } 
 # Main script
 
 echo "Listing users with read access to ${REPO_OWNER}/${REPO_NAME}..."
